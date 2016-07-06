41d3d72bcd10e12a97aa3505902912a7c64eec90
Macário Soares <macario.junior@gmail.com>
2016-07-06 15:51:00 -0300

    Finishing plugin conversion

--- AtaOnline/pdfConverter/views.py
+++ AtaOnline/pdfConverter/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""Module of PDF converter."""
+from xhtml2pdf import pisa
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 
-# Create your views here.
+
+class PdfConverter:
+    """Class to convert html page to a pdf file."""
+
+    def convert(page, output, data):
+        """Function to convert."""
+        target = get_template(page)
+        html = target.render(Context(data))
+
+        file = open(output, "w+b")
+        pisa.CreatePDF(html.encode('utf-8'), dest=file, encoding='utf-8')
+
+        file.seek(0)
+        pdf = file.read()
+        file.close()
+        return HttpResponse(pdf, 'application/pdf')
